29db3b5d5dfc31b2fc0f7681bbf2b61053d0249f
OrbitalGarden <orbitalgarden@outlook.com>
2025-09-08 22:13:45 +0200

    added shot, shoot function, and cleaned up code with solution files

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 import pygame
+import sys
 from constants import *
-from circleshape import *
 from player import *
 from asteroid import *
 from asteroidfield import *
-import sys
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
+
         for object in drawable:
             object.draw(screen)
+
         updatable.update(dt)
+
         for object in asteroids:
             if object.collision(player) == True:
                 print("Game Over!")
                 sys.exit(0)
+
         pygame.display.flip()
+
         dt = clock.tick(60.0) / 1000
 
 
